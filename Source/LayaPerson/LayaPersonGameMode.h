5304d1d5eb0f5e785fc17a6ae0334054c6a668d9
thinkthen <302842818@qq.com>
2024-09-12 23:47:59 +0800

    init project

---
+++ Source/LayaPerson/LayaPersonGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LayaPersonGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALayaPersonGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALayaPersonGameMode();
+};
+
+
+
